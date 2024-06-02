82ba506e600ea0179306e2cde6cfdd1193f7d438
Abdullah Al Mamun <abdullahalmamun1804064@gmail.com>
2024-06-03 02:00:50 +0600

    first commit

---
+++ models/booking.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide the patient ID'],
+  },
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide the doctor ID'],
+  },
+  appointmentDate: {
+    type: String,
+    required: [true, 'Please provide the appointment date'],
+  },
+  appointmentTime: {
+    type: String,
+    required: [true, 'Please provide the appointment time'],
+  },
+  reason: {
+    type: String,
+    required: [true, 'Please provide the reason for the appointment'],
+    maxLength: [100, 'Reason cannot exceed 100 characters'],
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'completed', 'cancelled'],
+    default: 'pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
